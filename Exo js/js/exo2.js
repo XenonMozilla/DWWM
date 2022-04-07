d8cfe8ac27973da9d9a53d5e5ef938094ddc166c
27010-34-13 <27010-34-13@exchange.ad.afpanet>
2022-04-07 16:26:06 +0200

    aaaaa le js

---
+++ Exo js/js/exo2.js
@@ -0,0 +1,9 @@
+var nmb1 = prompt();
+var nmb2 = prompt();
+
+nmb1 = parseInt(nmb1);
+nmb2 = parseInt(nmb2);
+
+nmb1 = nmb1 + nmb2;
+
+alert(nmb1);
---
+++ Exo js/js/exo4.js
@@ -0,0 +1,11 @@
+var nbr
+var stock = "les nombre premier son"
+
+for(var i = 100;){
+    i = i+1
+    if(nbr%i === 0){
+        stock = stock + nbr
+    }
+}
+
+alert
