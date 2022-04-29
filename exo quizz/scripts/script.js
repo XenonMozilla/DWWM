b90a6eb5b23e8f297a3f77680b8a6cec44e3122e
27010-34-13 <27010-34-13@exchange.ad.afpanet>
2022-04-29 11:47:14 +0200

    29/04

---
+++ exo quizz/scripts/script.js
@@ -0,0 +1,12 @@
+
+// var boucle = function(){
+//     var compteur = 0;
+//     var cliquer = function(){
+//         compteur += 1;
+//     }
+//     while(compteur != 1){
+//         console.log(compteur)
+//     }
+// }
+
+  var timer=setInterval("console.log('ay')", 250);
